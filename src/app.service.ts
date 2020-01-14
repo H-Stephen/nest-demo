47282ac0151cda6473a16a2bfa1b11a6d4279cbb
caizhanhao <caizhanhao@deepexi.com>
2020-01-14 15:05:15 +0800

    feat: 添加user路由

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,11 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  getUser(): object {
+    return {
+      code: 200,
+      message:"success",
+      payload:true
+    };
+  }
 }
